a3c821131acdb124a92f3a210c732ccf6c61791d
Alexis <pseudoalex@outlook.fr>
2025-01-20 22:44:22 +0100

    partenaire section

--- src/app/features/shop/shop.component.ts
+++ src/app/features/shop/shop.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  
+  partenairesValue : any
 
+  partenaires = [
+    {label: 'redbull', link: '../../../assets/images/redbull.avif'  },
+    {label: 'celio', link: '../../../assets/images/celio.avif'  },
+    {label: 'cic', link: '../../../assets/images/cic.jpg'  },
+    {label: 'fitness', link: '../../../assets/images/fitness.avif'  },
+    {label: 'logitech', link: '../../../assets/images/logitech.avif'  },
+    {label: 'michelin', link: '../../../assets/images/michelin.avif'  },
+    {label: 'orange', link: '../../../assets/images/orange.avif'  },
+    {label: 'quersus', link: '../../../assets/images/quersus.avif'  },
+    {label: 'rhino', link: '../../../assets/images/rhino.avif'  },
+    {label: 'ultragear', link: '../../../assets/images/ultragear.avif'  },
+  ]
+  
+
+
+  updateBasedOnWidth() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 768) {
+      this.partenairesValue = this.partenaires.slice(0,2); 
+    } else if (screenWidth < 1024) {
+      this.partenairesValue = this.partenaires.slice(0,4); 
+    } else {
+      this.partenairesValue = this.partenaires.slice(0,6); 
+    }
+  }
+
+  ngOnInit(): void {
+    this.updateBasedOnWidth()
+  }
 }
