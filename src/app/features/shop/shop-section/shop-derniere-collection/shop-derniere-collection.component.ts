c9df844acc6d7b5b8d1c360d1bb686b45033cec8
Alexis <pseudoalex@outlook.fr>
2025-01-25 18:09:30 +0100

    end of 'last collection' section

--- src/app/features/shop/shop-section/shop-derniere-collection/shop-derniere-collection.component.ts
+++ src/app/features/shop/shop-section/shop-derniere-collection/shop-derniere-collection.component.ts
@@ -8,20 +8,41 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 
 export class ShopDerniereCollectionComponent implements AfterViewInit {
 
-  typeCollection = [
-    {label: 'Nos produits', onIt: true },
-    {label: 'Chroma', onIt: false  },
-    {label: 'Accessoires', onIt: false  },
-  ]
+
 
   nosProduitsImg = [
     {imgUrl: '../../../../../assets/images/4_2025.webp', title: ' Maillot Pro 2025', subtitle: 'Dès €80,00'},
-    {imgUrl: '../../../../../assets/images/Frontultra.webp' , title: ' Maillot Ultra 2025', subtitle: 'Dès €65,00'},
-    {imgUrl: '../../../../../assets/images/Marteen_Veste.webp' , title: ' Veste Pro Kit 2025', subtitle: 'Dès €90,00'},
+    {imgUrl: '../../../../../assets/images/Frontultra.webp' , title: ' Maillot Ultra 2025', subtitle: '€65,00'},
+    {imgUrl: '../../../../../assets/images/Marteen_Veste.webp' , title: ' Veste Pro Kit 2025', subtitle: '€90,00'},
+    {imgUrl: '../../../../../assets/images/JoggFront2025.webp', title: ' Jogging Pro Kit 2025', subtitle: '€60,00'},
+    {imgUrl: '../../../../../assets/images/DUO_LOL_1.webp' , title: ' Précommande - Maillot League Of its Own', subtitle: 'Dès €75,00'},
+    {imgUrl: '../../../../../assets/images/Ava_1.webp' , title: 'Polo Chroma Noir', subtitle: '€70,00'},
   ]
+
+  chromaImg = [
+    {imgUrl: '../../../../../assets/images/Emilie3.webp', title: ' Polo Chroma Blanc', subtitle: '€70,00'},
+    {imgUrl: '../../../../../assets/images/Ava_1.webp' , title: 'Polo Chroma Noir', subtitle: '€70,00'},
+  ]
+
+  Accessoires = [
+    {imgUrl: '../../../../../assets/images/pins-dore-bois.webp', title: "Pin's Karmine Doré", subtitle: '€4,50'},
+    {imgUrl: '../../../../../assets/images/pinsdore_blackbois.webp', title: "Pin's Karmine Noir", subtitle: '€4,50'},
+    {imgUrl: '../../../../../assets/images/pins-bleu-bois.webp', title: "Pin's Karmine Navy", subtitle: '€4,50'},
+    {imgUrl: '../../../../../assets/images/kcwin-noir.jpg', title: "Porte clé #KCWIN Noir", subtitle: '€6,00'},
+    {imgUrl: '../../../../../assets/images/kcorp-bleu.jpg', title: "Porte clé Karmin Noir", subtitle: '€6,00'},
+  ]
+
+  typeCollection = [
+    {label: 'Nos produits', onIt: true, array: this.nosProduitsImg },
+    {label: 'Chroma', onIt: false,  array: this.chromaImg},
+    {label: 'Accessoires', onIt: false, array: this.Accessoires },
+  ]
+
+  arrayImgToDisplay = this.nosProduitsImg;
 
 
   onTypeCollection(label: string){
+    // all the other are false
     const indexOld = this.typeCollection.findIndex(item => item.onIt === true);
     if (indexOld !== -1) {
       this.typeCollection[indexOld].onIt = false;
@@ -29,16 +50,53 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
     const indexNew = this.typeCollection.findIndex(item => item.label === label);
     if (indexNew !== -1) {
       this.typeCollection[indexNew].onIt = true;
+      if (indexNew === 0) {
+        this.mainCarousel = this.carousel0;
+        this.mainFirstImg = this.firstImg0;
+        this.indexImg = 0;
+        this.updateBasedOnWidth(indexNew);
+        this.smoothScroll(0);
+      } 
+      if (indexNew === 1) {
+        this.mainCarousel = this.carousel1;
+        this.mainFirstImg = this.firstImg1;
+        this.indexImg = 0;
+        this.updateBasedOnWidth(indexNew);
+        this.smoothScroll(0);
+      } 
+      if (indexNew === 2) {
+        this.mainCarousel = this.carousel2;
+        this.mainFirstImg = this.firstImg2;
+        this.indexImg = 0;
+        this.updateBasedOnWidth(indexNew);
+        this.smoothScroll(0);
+      } 
     }
-    console.log(this.typeCollection);
+    
   }
   
-  carousel: any;
-  firstImg: any;
-  container: any;
-  @ViewChild('carousel') carouselElementRef!: ElementRef;
-  @ViewChild('firstImg') firstImgElementRef!: ElementRef;
-  @ViewChild('container') containerElementRef!: ElementRef;
+  carousel0: any;
+  carousel1: any;
+  carousel2: any;
+  firstImg0: any;
+  firstImg1: any;
+  firstImg2: any;
+  container0: any;
+  container1: any;
+  container2: any;
+  @ViewChild('carousel0') carouselElementRef0!: ElementRef;
+  @ViewChild('carousel1') carouselElementRef1!: ElementRef;
+  @ViewChild('carousel2') carouselElementRef2!: ElementRef;
+  @ViewChild('firstImg0') firstImgElementRef0!: ElementRef;
+  @ViewChild('firstImg1') firstImgElementRef1!: ElementRef;
+  @ViewChild('firstImg2') firstImgElementRef2!: ElementRef;
+  @ViewChild('container0') containerElementRef0!: ElementRef;
+  @ViewChild('container1') containerElementRef1!: ElementRef;
+  @ViewChild('container2') containerElementRef2!: ElementRef;
+
+  mainCarousel :any;
+  mainContainer :any;
+  mainFirstImg: any
   
 
   isDrageStart = false;
@@ -48,9 +106,11 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
   positionDiff: any
   dragSmooth = true;
   indexImg = 0;
-  
+  numOfbtn : any;
+  marginRight : number = 25;
+
   onMouseMove(e: MouseEvent | TouchEvent) {
-    if (this.carousel) {
+    if (this.mainCarousel) {
       if (!this.isDrageStart) return
       e.preventDefault();
       this.isDragging = true;
@@ -62,7 +122,7 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
         // TouchEvent has touches[0]
         this.positionDiff = (e.touches[0].pageX) - this.prevPageX;
       }
-      this.carousel.scrollLeft = this.prevScrollLeft - this.positionDiff;
+      this.mainCarousel.scrollLeft = this.prevScrollLeft - this.positionDiff;
     }
   }
 
@@ -75,33 +135,36 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
     } else if (e instanceof TouchEvent) {
       this.prevPageX = e.touches[0].pageX ;
     }
-    this.prevScrollLeft = this.carousel.scrollLeft;
+    this.prevScrollLeft = this.mainCarousel.scrollLeft;
   }
 
   autoSlide() {
 
-    if (this.carousel.scrollLeft == (this.carousel.scrollWidth - this.carousel.clientWidth)) {
+    if (this.mainCarousel.scrollLeft == (this.mainCarousel.scrollWidth - this.mainCarousel.clientWidth)) {
       return;
     } 
 
 
     this.positionDiff = Math.abs(this.positionDiff);
-    let marginRightCalc = (this.container.getBoundingClientRect().width - this.firstImg.getBoundingClientRect().width)/2;
-    let firstImgWidth = this.firstImg.getBoundingClientRect().width + 10
+    // let marginRightCalc = (this.container.getBoundingClientRect().width - this.firstImg.getBoundingClientRect().width)/2;
+    let firstImgWidth = this.mainFirstImg.getBoundingClientRect().width + this.marginRight;
+    console.log(firstImgWidth);
     let valDifference = firstImgWidth - this.positionDiff;
 
     // right
-    if (this.carousel.scrollLeft > this.prevScrollLeft) {
+    if (this.mainCarousel.scrollLeft > this.prevScrollLeft) {
       // full on the right don't go further
       if (this.indexImg === this.nosProduitsImg.length-1) return
       if (this.positionDiff > firstImgWidth / 3) {
-        const targetScrollLeft = this.carousel.scrollLeft + valDifference
+        // const targetScrollLeft = this.mainCarousel.scrollLeft + valDifference;
         this.indexImg++;
+        const targetScrollLeft = firstImgWidth*this.indexImg;
         this.smoothScroll(targetScrollLeft);
       }
       else {
         if (this.indexImg === this.nosProduitsImg.length-1) return
-        const targetScrollLeft = this.carousel.scrollLeft + (-this.positionDiff)
+        // const targetScrollLeft = this.mainCarousel.scrollLeft + (-this.positionDiff)
+        const targetScrollLeft = firstImgWidth*this.indexImg;
         this.smoothScroll(targetScrollLeft);
       }
       return
@@ -110,14 +173,15 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
     if (this.positionDiff > firstImgWidth / 3) {
       // full on the left don't go further
       if (this.indexImg === 0) return
-      const targetScrollLeft = this.carousel.scrollLeft - valDifference
-      console.log(targetScrollLeft);
+      // const targetScrollLeft = this.mainCarousel.scrollLeft - valDifference;
       this.indexImg--;
+      const targetScrollLeft = firstImgWidth*this.indexImg;
       this.smoothScroll(targetScrollLeft);
     }
     else {
       if (this.indexImg === 0) return
-      const targetScrollLeft = this.carousel.scrollLeft - (-this.positionDiff)
+      // const targetScrollLeft = this.mainCarousel.scrollLeft - (-this.positionDiff)
+      const targetScrollLeft = firstImgWidth*this.indexImg;
       this.smoothScroll(targetScrollLeft);
     }
   }
@@ -131,7 +195,7 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
   }
 
   smoothScroll(targetScrollLeft: number) {
-    const startScrollLeft = this.carousel.scrollLeft;
+    const startScrollLeft = this.mainCarousel.scrollLeft;
     const distance = targetScrollLeft - startScrollLeft;
     const duration = 500; // Duration of the animation in milliseconds
     const startTime = performance.now();
@@ -144,7 +208,7 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
       const ease = 1 - Math.pow(1 - progress, 3);
   
       // Update the scrollLeft position
-      this.carousel.scrollLeft = startScrollLeft + distance * ease;
+      this.mainCarousel.scrollLeft = startScrollLeft + distance * ease;
   
       if (progress < 1) {
         window.requestAnimationFrame(scrollStep);
@@ -155,8 +219,11 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
   }
 
   setIndexImg(index: number){
+    console.log(this.nosProduitsImg.length - 1);
+    console.log(this.indexImg);
+
     const diffIndex = index - this.indexImg;
-    const targetScrollLeft = index*(this.firstImg.getBoundingClientRect().width+10)
+    const targetScrollLeft = index*(this.mainFirstImg.getBoundingClientRect().width+this.marginRight)
     if (diffIndex===0) {
       return
     }
@@ -170,12 +237,47 @@ export class ShopDerniereCollectionComponent implements AfterViewInit {
     this.smoothScroll(targetScrollLeft);
   }
 
+  updateBasedOnWidth(index: number) {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth < 768) {
+      this.marginRight = 10;
+      this.numOfbtn = Array.from({ length:  this.typeCollection[index].array.length }, (_, i) => i + 1);
+    } 
+    else {
+      if (screenWidth < 1024) {
+        this.marginRight = 25;
+      } else {
+        this.marginRight = 40;
+      }
+
+      let count =  this.typeCollection[index].array.length - 2;
+      if (count < 2) {
+        count = 0
+      }
+      this.numOfbtn = Array.from({ length: count }, (_, i) => i + 1);
+    }
+  }
+
+  ngOnInit(): void {
+    this.updateBasedOnWidth(0)
+  }
+
 
   ngAfterViewInit(): void {
     // this.carousel = document.querySelector(".carousel");
-    this.carousel = this.carouselElementRef.nativeElement;
-    this.firstImg = this.firstImgElementRef.nativeElement;
-    this.container = this.containerElementRef.nativeElement;
+    this.carousel0 = this.carouselElementRef0.nativeElement;
+    this.carousel1 = this.carouselElementRef1.nativeElement;
+    this.carousel2 = this.carouselElementRef2.nativeElement;
+    this.firstImg0 = this.firstImgElementRef0.nativeElement;
+    this.firstImg1 = this.firstImgElementRef1.nativeElement;
+    this.firstImg2 = this.firstImgElementRef2.nativeElement;
+    this.container0 = this.containerElementRef0.nativeElement;
+    this.container1 = this.containerElementRef1.nativeElement;
+    this.container2 = this.containerElementRef2.nativeElement;
+    this.mainCarousel = this.carousel0;
+    this.mainContainer = this.container0;
+    this.mainFirstImg = this.firstImg0;
   }
 
 }
